90e88c169807f03efae1c4ad5d53a374a41dbb46
Kennedy Nguyen <kennedyn216@gmail.com>
2026-06-29 12:12:30 -0500

    drew my first ever triangle

---
+++ shaders/triangle.vert
@@ -0,0 +1,13 @@
+#version 330 core
+
+// expect a 3d input at attrivute location 0
+layout (location = 0) in vec3 aPos;
+
+
+void main() {
+
+    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0); // this can also be just vec4(aPos, 1.0)
+
+
+
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <fstream> 
+#include <sstream>
+#include <string>
 
 
-void processInput(GLFWwindow *window) {
+void processInput(GLFWwindow* window) {
     if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS){
         glfwSetWindowShouldClose(window,true);
     }
@@ -11,10 +14,32 @@ void processInput(GLFWwindow *window) {
 
 void framebuffer_size_callback(GLFWwindow*, int width, int height){
         glViewport(0, 0, width, height);
-    };
+    }
+
+//this is opens the triangle vert and takes all the glsl code
+// and returns it as a c++ string 
+std::string readFile(const char* path) 
+{
+    std::ifstream file(path);
+    std::stringstream buffer;
+
+    buffer << file.rdbuf();
+
+    return buffer.str();
+
+}
+
+
 
 int main()
 {   
+// setting vertices of triangle
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+         0.5f,  0.5f, 0.0f
+    };
+
     // this is initializng and then telling what version i want 
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -32,10 +57,46 @@ int main()
     glfwMakeContextCurrent(window);
 //loads function pointers
     if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
-
         std::cout << "Failed to initialize GLAD" << std:: endl;
         return -1;
     };
+    unsigned int VBO;
+    unsigned int VAO;
+
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1,&VBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+//lets make a triangle and start coloring ts
+    std::string vertexCode = readFile("shaders/triangle.vert");
+    std::string fragmentCode = readFile("shaders/triangle.frag");
+    const char* vertexShaderSource = vertexCode.c_str();
+    const char* fragmentShaderSource = fragmentCode.c_str();
+    //attaches shader text to object. not giving exact string lengths just until null teriminator
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+    glCompileShader(vertexShader);
+
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+    glCompileShader(fragmentShader);
+
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(fragmentShader);
+    glDeleteShader(vertexShader);
+
+
+
 // if the window size changes cal this functon so opengl updates its viewport
     glfwSetFramebufferSizeCallback(window,framebuffer_size_callback);
 
@@ -43,15 +104,15 @@ int main()
     while(!glfwWindowShouldClose(window)) {
              // constantly updates and listens for key presses
         processInput(window);
-
-
+       
         // im finna render 
         // sets the screen to be a dark blue color 
         glClearColor(0.02f, 0.04f, 0.08f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-
-
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES,0, 3);
 
 
         //shows newly drawn frame
@@ -60,7 +121,7 @@ int main()
         glfwPollEvents();
         
     }
-
+    glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
 }
