9ee9a493e0eb3411114c4b58bc44b7825f83ef3d
Kennedy Nguyen <kennedyn216@gmail.com>
2026-06-30 16:01:59 -0500

    Add indexed rectangle rendering with EBO

--- src/main.cpp
+++ src/main.cpp
@@ -38,11 +38,17 @@ std::string readFile(const char* path)
 
 int main()
 {   
-// setting vertices of triangle
+// setting vertices of rectangle
     float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
+         0.5f,  0.5f, 0.0f,
          0.5f, -0.5f, 0.0f,
-         0.0f,  0.5f, 0.0f
+        -0.5f, -0.5f, 0.0f,
+        -0.5f,  0.5f, 0.0f
+    };
+
+    unsigned int indices[] = {
+        0,1,3, // this is the first triangle
+        1,2,3 // this is the second triangle
     };
 
     // this is initializng
@@ -70,16 +76,24 @@ int main()
     };
 // vertex buffer object stores vertex data
 // vertex buffer array remmebers how to interpret vertex data
+//elemental buffer remmebers how to use the vertices
     unsigned int VBO;
     unsigned int VAO;
+    unsigned int EBO;
+
 
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1,&VBO);
+    glGenBuffers(1, &EBO);
+
 
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
@@ -150,7 +164,7 @@ int main()
 
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES,0, 3);
+        glDrawElements(GL_TRIANGLES,6,GL_UNSIGNED_INT, 0);
 
 
         //shows newly drawn frame
@@ -163,6 +177,7 @@ int main()
 
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     glDeleteProgram(shaderProgram);
     glfwDestroyWindow(window);
     glfwTerminate();
